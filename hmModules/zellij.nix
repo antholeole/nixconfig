41cb0b062e74b0fc8213eca6aad7313651ccac54
Anthony Oleinik <antholeinik@gmail.com>
2023-06-23 10:41:28 -0500

    all but helix

---
+++ hmModules/zellij.nix
@@ -0,0 +1,44 @@
+{ inputs, ... }: let
+  colors = import ../theme.nix;
+  layoutDir = ".config/zellij/layouts";
+ in  {
+  programs.zellij = {
+    enable = true;
+
+    settings = {
+      theme = "catpuccin";
+
+      pane_frames = false;
+      mouse_mode = false;
+
+      default_layout = "~/${layoutDir}/custom_2_tab.kdl";
+      
+      ui = {
+        pane_frames = {
+          hide_session_name = false;
+        };
+      };
+      
+      
+      themes = {
+        catpuccin = with colors; {
+          inherit red green blue yellow;
+          
+          bg = surface2;
+          fg = text;
+
+          magenta = pink;
+          orange = peach;
+          cyan = sky;
+          black = mantle;
+          white = text;
+        };
+      };
+    };
+  };
+
+  home.file."${layoutDir}" = {
+    recursive = true;
+    source = "${inputs.self}/confs/zellij/layouts";
+  };
+}
