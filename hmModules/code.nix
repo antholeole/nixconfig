b153a5c40c1ae0009d94f1a15401929cde2e3332
Anthony Oleinik <antholeinik@gmail.com>
2023-07-15 13:41:49 -0500

    attempting harpoon

--- hmModules/code.nix
+++ hmModules/code.nix
@@ -7,12 +7,13 @@
         # This is much better than the complex setup
         rust-lang.rust-analyzer
         # gregoire.dance :(
+        # tobias-z.vscode-harpoon :(
         catppuccin.catppuccin-vsc
         # catppuccin.catppuccin-vsc-icons
         yzhang.markdown-all-in-one
       ];
 
-    keybindings = with builtins; fromJSON (readFile "${inputs.self}/confs/code/keybindings.json");
-    userSettings = with builtins; fromJSON (readFile "${inputs.self}/confs/code/settings.json");
+    # keybindings = with builtins; fromJSON (readFile "${inputs.self}/confs/code/keybindings.json");
+    # userSettings = with builtins; fromJSON (readFile "${inputs.self}/confs/code/settings.json");
   };
 }
