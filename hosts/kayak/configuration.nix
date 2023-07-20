878cf64c582170dca13474da084e2c7715114ca2
Anthony Oleinik <antholeinik@gmail.com>
2023-07-19 20:26:30 -0700

    tz

--- hosts/kayak/configuration.nix
+++ hosts/kayak/configuration.nix
@@ -30,7 +30,7 @@
   
   networking.networkmanager.enable = true;
 
-  time.timeZone = "America/Chicago";
+  time.timeZone = "US/Pacific";
 
   i18n.defaultLocale = "en_US.UTF-8";
   users.mutableUsers = false;
