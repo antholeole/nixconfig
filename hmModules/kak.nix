86df2359598db9f36c965b50eb511c1e38f2f862
Anthony Oleinik <oleina@google.com>
2023-08-11 21:04:02 -0700

    simplified extending the kanshi config

--- hmModules/kak.nix
+++ hmModules/kak.nix
@@ -2,5 +2,12 @@
 {
   programs.kakoune = {
     enable = true;
+
+    #config = {
+    #  hooks = [{
+    #    name = "RegisterModified";
+    #    commands =  "printf %s \"$kak_main_reg_dquote\" | ${pkgs.wl-clipboard}/bin/wl-copy > /dev/null 2>&1 &";
+    #   }];
+    #};
   };
 }
--- hmModules/kanshi.nix
+++ hmModules/kanshi.nix
@@ -2,27 +2,46 @@
   services.kanshi = lib.mkIf (!sysConfig.headless) {
     enable = true;
 
-    profiles = {
-        dellUltrawide = let 
-          monitor = "DP-2";
-          laptop = "eDP-1";
-        in {
-          outputs = [
-            {
-              criteria = laptop; # laptop
-            }
-            {
-              criteria = monitor;
-              mode = "2560x1440@59.951Hz";
-              scale = 1.0;
-            }
-          ];
-
-          exec = [
-            "${pkgs.swaybg}/bin/swaybg -i ~/wall.png -c \"#1e2030\" -m center -o ${laptop}"
-            "${pkgs.swaybg}/bin/swaybg -i ~/wall_tall.png -c \"#1e2030\" -m center -o ${monitor}"
-          ];
+    profiles =
+      let
+        mkBgStatement = criteria: if criteria.tall then 
+            "${pkgs.swaybg}/bin/swaybg -i ~/wall_tall.png -c \"#1e2030\" -m center -o ${criteria.kanshi.criteria}"
+         else 
+            "${pkgs.swaybg}/bin/swaybg -i ~/wall.png -c \"#1e2030\" -m center -o ${criteria.kanshi.criteria}";
+        ultraWide = {
+          tall = true;
+          kanshi = {
+            criteria = "DP-2";
+            mode = "2560x1440@59.951Hz";
+            scale = 1.0;
+          };
         };
-    };
+        laptop = {
+          tall = false;
+          kanshi = {
+            criteria = "eDP-1";
+          };
+        };
+        monitor = {
+          tall = true;
+          kanshi = {
+            criteria = "HDMI-A-1";
+          };
+        };
+        mkKanshiConfig = displays: with builtins; {
+          outputs = map (display: display.kanshi) displays;
+          exec = map mkBgStatement displays; 
+        };
+      in
+      {
+        dellUltrawide = mkKanshiConfig [
+          ultraWide
+          laptop
+        ];
+        second = mkKanshiConfig [
+          laptop
+          monitor
+        ];
+      };
   };
 }
