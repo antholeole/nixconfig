bb7278915939f5e49c955b54442688bada3e6532
oleina <oleina@google.com>
2023-11-20 08:30:44 -0800

    netcat

--- mixins/mkSystemCopy.nix
+++ mixins/mkSystemCopy.nix
@@ -1,4 +1,4 @@
 sysConfig: pkgs: if sysConfig.headless then
- with pkgs; "${netcat}/bin/netcat -w 0 localhost 9791"
+ with pkgs; "${netcat-gnu}/bin/nc -w 0 localhost 9791"
 else
  "${pkgs.wl-clipboard.outPath}/bin/wl-copy"
