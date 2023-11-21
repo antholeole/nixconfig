19a69221ffbe101884ef4ced7e9a9466f1c36e43
oleina <oleina@google.com>
2023-11-21 08:52:39 -0800

    starship

--- hmModules/starship.nix
+++ hmModules/starship.nix
@@ -23,7 +23,7 @@
 
     settings = {
       format = "$character";
-      right_format = "$git_branch $git_metrics $directory┃$hostname";
+      right_format = "$git_branch $git_metrics $directory$hostname";
       
       directory = { format = "[  ┃ $path ](${bgFill})"; };
       cmd_duration = { format = "${seperator}[$duration](${bgFill})"; };
@@ -40,7 +40,7 @@
       hostname = {
         ssh_symbol = "󱘖 ";
         disabled = false;
-        format = "[ $ssh_symbol ](${bgFill})";
+        format = "[┃](${fgFill})[ $ssh_symbol ](${bgFill})";
       };
 
       character = {
