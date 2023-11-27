b335866decefeb3cf769bd0679d9fd6a84a4aa24
oleina <oleina@google.com>
2023-11-26 22:18:24 -0800

    as long as you don't push anything it works

--- confs/ags/bar.js
+++ confs/ags/bar.js
@@ -1,5 +1,6 @@
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
 import Hyprland from 'resource:///com/github/Aylur/ags/service/hyprland.js';
+import Battery from 'resource:///com/github/Aylur/ags/service/battery.js';
 import { execAsync } from 'resource:///com/github/Aylur/ags/utils.js';
 
 const Clock = () => Widget.Box({
@@ -20,18 +21,19 @@ const Workspaces = (monitor) => Widget.Box({
         const thisMonitor = Hyprland.getMonitor(monitor)
         const workspaces = Hyprland.workspaces.filter((workspace) => workspace.monitor === thisMonitor.name)
 
-        const isActive =
+        const active = workspace => thisMonitor.activeWorkspace.id === workspace.id && Hyprland.active.monitor == thisMonitor.name
+        const showText = false
 
-            self.children = workspaces.map(workspace => Widget.Box({
-                className: (thisMonitor.activeWorkspace.id === workspace.id && Hyprland.active.monitor == thisMonitor.name) ? "dot-full" : "dot-empty",
-                children: []
-            }));
+        self.children = workspaces.map(workspace => Widget.Box({
+            className: showText ? "ws-text" : `dot-${active(workspace) ? 'full' : 'empty'}`,
+            children: showText ? [Widget.Label({
+                label: `${workspace.id}`
+            })] : []
+        }));
     }]],
 });
 
-
-
-export default (monitor) => Widget.Window({
+export const Bar = (monitor) => Widget.Window({
     name: `bar-${monitor}`,
     monitor,
     anchor: ['top', 'left', 'right'],
@@ -40,6 +42,12 @@ export default (monitor) => Widget.Window({
         Widget.CenterBox({
             className: 'bar',
             startWidget: Workspaces(monitor),
-            endWidget: Clock()
+            endWidget: Widget.Box({
+                hpack: 'end',
+                children: [
+                    Clock()
+                ]
+
+            })
         })
 })
--- confs/ags/config.js
+++ confs/ags/config.js
@@ -1,12 +1,17 @@
 import App from 'resource:///com/github/Aylur/ags/app.js';
 import Powerbar from "./powerbar.js"
-import Bar from "./bar.js"
+import { Bar } from "./bar.js"
+
+const Blah = () => {
+    return "asdja"
+}
 
 
 export default {
     style: App.configDir + "/style.css",
+
     windows: [
-        Powerbar(0),
+        // Powerbar(0),
         Bar(0),
         Bar(1)
     ]
