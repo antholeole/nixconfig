7d04e508430d15b9b1042568b0d867904e5e4867
oleina <oleina@google.com>
2023-12-11 19:57:04 -0800

    this is scuffed

--- hmModules/anthony.nix
+++ hmModules/anthony.nix
@@ -51,6 +51,11 @@
       source = "${inputs.self}/assets/id_ed25519.pub";
     };
 
+    file.".ssh/config" = {
+      enable = true;
+      source = "${inputs.self}/assets/config";
+    };
+
     file.".ssh/authorized_keys" = {
       enable = true;
       source = "${inputs.self}/assets/authorized_keys";
