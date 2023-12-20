daf267ea770283fbaaa56bae556dc434b32c3de9
oleina <oleina@google.com>
2023-12-20 10:52:38 -0800

    more

--- confs/services/clipboard/default.nix
+++ confs/services/clipboard/default.nix
@@ -1,22 +1,30 @@
 pkgs:
 let
-  remoteClipPkg = path: vendorHash: pkgs.buildGoModule {
-      vendorHash = "sha256-eDSdZpN51IlQ6TcikkBF0BprOmkjp3+mY5A5VC37RDU=";
+  fs = pkgs.lib.fileset;
 
-      modRoot = ./.;
-      src = ./.;
-      
-      pname = "oleinaconf.com";
-      name = "oleinaconf.com";
+  remoteClipPkg = filterOut: vendorHash:
+    let fileset = fs.fileFilter (file: file.name != filterOut) ./.;
+    in pkgs.buildGoModule {
+      inherit vendorHash;
+      src = fs.toSource {
+        inherit fileset;
+
+        root = ./.;
+      };
+
+      name = "remoteclipboard";
     };
 in {
-  server = remoteClipPkg "server" "";
+  # this is confusing but its much simpler to specify the file 
+  # that we don't want 
+  server = remoteClipPkg "client.go"
+    "sha256-KMsYgkLRoQvxDJQvqC58zOyUlU2Kc1sx0s0EbDDGIVQ=";
   client = let
-    rcClient = remoteClipPkg "client" pkgs.lib.fakeHash;
+    rcClient = remoteClipPkg "server.go"
+      "sha256-eDSdZpN51IlQ6TcikkBF0BprOmkjp3+mY5A5VC37RDU=";
   in {
-    copy = "${remoteClipPkg}/bin/oleinaconf.com copy";
-    paste = "${remoteClipPkg}/bin/oleinaconf.com paste";
-    cliphist = "${remoteClipPkg}/bin/oleinaconf.com cliphist";
-    done = "${remoteClipPkg}/bin/oleinaconf.com done";
+    copy = "${rcClient}/bin/oleinaconf.com copy";
+    paste = "${rcClient}/bin/oleinaconf.com paste";
+    cliphist = "${rcClient}/bin/oleinaconf.com cliphist";
   };
 }
