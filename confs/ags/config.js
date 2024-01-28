e89b3306098092d0a80ccb247e1ada5580a05234
oleina <oleina@google.com>
2024-01-28 08:27:42 -0800

    kind of works

--- confs/ags/config.js
+++ confs/ags/config.js
@@ -1,23 +1,16 @@
 import App from 'resource:///com/github/Aylur/ags/app.js'
-import { exec } from 'resource:///com/github/Aylur/ags/utils.js'
 import { Bar } from "./bar.js"
 import "./launcher.js"
-import"./powerbar.js"
+import "./powerbar.js"
 import { NotificationBar } from './notifications.js'
 
-let windows = [
-    Bar(0),
-    NotificationBar(0)
-]
+import { monitorWatcher } from "./utils.js"
 
-// Hyprland does not populate here so syscall instead
-const monitors = JSON.parse(exec("hyprctl monitors -j"))
-if (monitors.length > 1) {
-    windows.push(Bar(1))
-    windows.push(NotificationBar(1))
-}
+monitorWatcher(App, [
+    Bar, 
+    NotificationBar
+])
 
 export default {
     style: App.configDir + "/style.css",
-    windows
 }
