e4423bc817255d8f05f1490a66a5120064d627ee
oleina <oleina@google.com>
2024-03-31 09:41:25 -0700

    just install normal rust

--- hmModules/langs.nix
+++ hmModules/langs.nix
@@ -3,10 +3,6 @@
   # on PATH. So we need these packages to be installed globally. 
   home.packages = with pkgs; [
     gcc
-    (rust-bin.stable.latest.default.override {
-      extensions = [ "rust-analyzer" "rust-src" ];
-      targets = [ "wasm32-wasi" ];
-    })
     nixpkgs-fmt
     picat
   ];
