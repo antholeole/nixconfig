d64a0da74feb310e51e0c6a8290cd52c8efb1c22
oleina <oleina@google.com>
2024-04-01 20:07:40 +0000

    idk y

--- hmModules/langs.nix
+++ hmModules/langs.nix
@@ -1,9 +1,5 @@
 { pkgs, ... }: {
   # unfortunately, for vscode extensions, executables need to be
   # on PATH. So we need these packages to be installed globally. 
-  home.packages = with pkgs; [
-    gcc
-    nixpkgs-fmt
-    picat
-  ];
+  home.packages = with pkgs; [ gcc nixpkgs-fmt picat ];
 }
