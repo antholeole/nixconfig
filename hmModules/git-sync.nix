4c931da9414b0b42e2385f85ae25be9520fc3851
oleina <oleina@google.com>
2024-04-21 20:50:05 -0700

    fixed gitsync notes

---
+++ hmModules/git-sync.nix
@@ -0,0 +1,11 @@
+{ config, ... }: {
+    services.git-sync = {
+        enable = true;
+        repositories = {
+            notebook = {
+                uri = "git@github.com:antholeole/notebook.git";
+                path = config.home.homeDirectory + "/Notes";
+            };
+        };
+    };
+}
