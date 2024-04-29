4b5f9a82142b4024f5bb4a634f5c68dd071f5a93
oleina <oleina@google.com>
2024-04-28 18:59:05 -0700

    added comment

--- hmModules/atuin.nix
+++ hmModules/atuin.nix
@@ -7,6 +7,8 @@
             style = "compact";
             show_help = false;
             inline_height = 10;
+
+            # not working?
             ctrl_n_shortcuts = true;
         };
     };
