3d3feddb7c7e074183dae9db9aa3a1c85c3ee81f
oleina <oleina@google.com>
2024-04-30 10:16:05 -0700

    updated nixpks

--- hmModules/atuin.nix
+++ hmModules/atuin.nix
@@ -3,10 +3,16 @@
         enable = true;
         enableFishIntegration = true;
 
+        flags = [
+          "--disable-up-arrow"
+        ];
+
         settings = {
             style = "compact";
             show_help = false;
             inline_height = 13;
+            invert = true;
+            enter_accept = true;
 
             # not working?
             ctrl_n_shortcuts = true;
