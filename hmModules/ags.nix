556dff964f659de135fd3420055d4746bc7c7c51
oleina <oleina@google.com>
2024-06-21 14:39:08 -0700

    got everything workng

--- hmModules/ags.nix
+++ hmModules/ags.nix
@@ -32,7 +32,7 @@
 
       # needs hyprland on path or it fails
       Environment =
-        "PATH=${pkgs.fzf}/bin:${pkgs.hyprland}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
+        "PATH=${pkgs.fzf}/bin:${config.wayland.windowManager.hyprland.package}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
       ExecStart = "${config.programs.ags.package}/bin/ags";
       ExecStartPre = "/bin/pkill ags";
     };
