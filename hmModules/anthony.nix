c15bfe4d3931e0894afb7a39f7164251d08c3f43
oleina <oleina@google.com>
2024-07-03 19:27:19 -0700

    remove atuin

--- hmModules/anthony.nix
+++ hmModules/anthony.nix
@@ -85,6 +85,7 @@ in {
         topfew-rs # fast!
         ncdu # see where our big folders are
         nixgl.auto.nixGLDefault # unboxing nix sad
+        gawk
         aspell aspellDicts.en
 
         # LANGUAGE SPECIFIC
