360ed846ea7d6a54e7703691513e525abe97a2f7
oleina <oleina@google.com>
2024-07-14 21:13:13 -0700

    ignored .jj
    Signed-off-by: oleina <oleina@google.com>
    Change-Id: 0a10699d2e3d07ebd71d6a1a9b207d363e21e886

--- hmModules/jujitsu.nix
+++ hmModules/jujitsu.nix
@@ -1,4 +1,42 @@
-{ lib, config, sysConfig, ... }: {
+{ lib, config, sysConfig, pkgs, ... }: let 
+  # TODO move this somewhere sensible.
+  jjBin = "${config.programs.jujutsu.package}/bin/jj";
+  jjSignoff = pkgs.writeShellScriptBin "jj-signoff" ''
+set -euo pipefail
+
+NAME=$(${jjBin} config get user.name)
+MAIL=$(${jjBin} config get user.email)
+CID=$(${jjBin} log --no-graph -r @ -T "change_id" | sha256sum | head -c 40)
+
+SIGNSTR="Signed-off-by: ''${NAME} <''${MAIL}>"
+CHGSTR="Change-Id: ''${CID}"
+
+contents=$(<"$1")
+readarray -t lines <<<"''${contents}"
+
+body=""
+last=""
+for x in "''${lines[@]}"; do
+  [[ "$x" =~ ^"JJ:" ]] && continue
+  [[ "$x" =~ ^"Change-Id:" ]] && continue
+  [[ "$x" =~ ^"$SIGNSTR" ]] && continue
+
+  [[ "$x" == "" ]] && [[ "$last" == "" ]] && continue
+
+  last="$x"
+  body+="$x\n"
+done
+
+body+="$SIGNSTR\n"
+body+="$CHGSTR\n"
+
+t=$(mktemp)
+printf "$body" > "$t"
+mv "$t" "$1"
+
+exec ${config.programs.kakoune.package}/bin/kak "$1"
+  '';
+in {
   programs.jujutsu = {
     enable = true;
 
@@ -12,6 +50,10 @@
       };
 
       git = { push-branch-prefix = "${sysConfig.selfAlias}/"; };
+
+      aliases = {
+        signoff = [ "--config-toml=ui.editor='${jjSignoff}/bin/jj-signoff'" "commit" ];
+      };
     };
 
   };
