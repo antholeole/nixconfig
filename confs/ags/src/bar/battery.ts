f7a6ea6c354979ae5d921c28bb8dcb952b683203
oleina <oleina@google.com>
2024-09-12 13:13:18 -0700

    made battery widget testable

--- confs/ags/src/bar/battery.ts
+++ confs/ags/src/bar/battery.ts
@@ -1,57 +1,75 @@
-import Battery from 'resource:///com/github/Aylur/ags/service/battery.js'
-import Widget from 'resource:///com/github/Aylur/ags/widget.js';
-import type Box from 'types/widgets/box';
-import type Label from 'types/widgets/label';
+import { battery as batteryService } from "resource:///com/github/Aylur/ags/service/battery.js";
+import Widget from "resource:///com/github/Aylur/ags/widget.js";
+import type { Connectable } from "types/service";
 
 type IBattery = {
-    percent: number,
-    charging: boolean,
+	percent: number;
+	charging: boolean;
+} & Omit<Connectable, "disconnect">;
+
+export const BATTERY_CONSTANTS = {
+	SHOW_UNDER_PERCENT: 80,
+	CHARGING_ICON: "󰂄",
+	BATTERIES: [
+		[99, "󰁹"],
+		[90, "󰂂"],
+		[80, "󰂁"],
+		[70, "󰂀"],
+		[60, "󰁿"],
+		[50, "󰁾"],
+		[40, "󰁽"],
+		[30, "󰁼"],
+		[20, "󰁻"],
+		[10, "󰁺"],
+		[0, "󱃍"],
+	],
 };
 
-export const BatteryBar = (battery: IBattery = Battery) => Widget.Box({
-    connections: [[battery, (self: Box<unknown, unknown>) => {
-        const children: Label<unknown>[] = []
+export const batteryProps = (
+	battery: IBattery,
+): {
+	percentLabel?: string;
+	icon: string;
+} => {
+	const ret: Partial<ReturnType<typeof batteryProps>> = {};
 
-        if (battery.percent < 80) {
-            children.push(Widget.Label({
-                class_name: "battery-label",
-                label: `${battery.percent.toString()}%`
-            }))
-        }
+	if (battery.percent < BATTERY_CONSTANTS.SHOW_UNDER_PERCENT) {
+		ret.percentLabel = `${battery.percent.toString()}`;
+	}
 
-        const batteries = [
-            [99, "󰁹"],
-            [90, "󰂂"],
-            [80, "󰂁"],
-            [70, "󰂀"],
-            [60, "󰁿"],
-            [50, "󰁾"],
-            [40, "󰁽"],
-            [30, "󰁼"],
-            [20, "󰁻"],
-            [10, "󰁺"],
-            [0, "󱃍"]
-        ]
+	if (battery.charging) {
+		ret.icon = BATTERY_CONSTANTS.CHARGING_ICON;
+	} else {
+		for (const [percent, icon] of BATTERY_CONSTANTS.BATTERIES) {
+			if ((battery.percent as number) >= (percent as number)) {
+				ret.icon = icon as string;
+				break;
+			}
+		}
+	}
 
-        let selectedIcon = ""
+	return ret as ReturnType<typeof batteryProps>;
+};
 
-        if (battery.charging) {
-            selectedIcon = "󰂄"
-        } else {
-            for (const [percent, icon] of batteries) {
-                if (battery.percent as number >= (percent as number)) {
-                    selectedIcon = icon as string
-                    break
-                }
-            }
-        }
+export const BatteryBar = (battery: IBattery = batteryService) =>
+	Widget.Box().hook(battery as unknown as Connectable, (self) => {
+		battery
+		const { percentLabel, icon } = batteryProps(battery);
 
-        children.push(Widget.Label({
-            class_name: "battery-icon",
-            label: selectedIcon
-        }))
+		const percentLabelWidget = percentLabel
+			? [
+					Widget.Label({
+						class_name: "battery-label",
+						label: `${battery.percent.toString()}%`,
+					}),
+				]
+			: [];
 
-
-        self.children = children
-    }]]
-});
+		self.children = [
+			...percentLabelWidget,
+			Widget.Label({
+				class_name: "battery-icon",
+				label: icon,
+			}),
+		];
+	});
