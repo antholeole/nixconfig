86cb3ee8ebc9289fa84a8ba47ff9c947672dc870
oleina <oleina@google.com>
2024-12-20 13:33:46 -0800

    begin notes script

--- hmModules/ags.nix
+++ hmModules/ags.nix
@@ -10,32 +10,55 @@
     palette = config.colorScheme.palette;
   }}/lib";
 in {
-  programs.ags = {
-    inherit configDir;
-    enable = !config.conf.headless;
-    extraPackages = with pkgs; [fzf hyprland];
+  config = {
+    programs.ags = {
+      inherit configDir;
+      extraPackages = [];
+      enable = !config.conf.headless;
+    };
+
+    home.file = {
+      ".config/data/launcher.json" = {
+        enable = !config.conf.headless;
+
+        text = with pkgs; let
+          broPleaseItsWaylandTrustMe = pgm: "DESKTOP_SESSION=hyprland XDG_SESSION_TYPE=wayland WAYLAND_DISPLAY=wayland-1 XDG_BACKEND=wayland ${pgm}";
+
+          # TODO: this probably shouldn't be in this file its hard to find
+          entries = {
+            "alacritty" = "${config.programs.alacritty.package}/bin/alacritty -e ${pkgs.zellij}/bin/zellij";
+            "code" =
+              broPleaseItsWaylandTrustMe
+              "${config.programs.vscode.package}/bin/code  --enable-features=UseOzonePlatform --ozone-platform=wayland";
+            "chrome" =
+              broPleaseItsWaylandTrustMe
+              "/bin/google-chrome  --enable-features=UseOzonePlatform --ozone-platform=wayland";
+            "qute" = "${config.programs.qutebrowser.package}/bin/qutebrowser";
+          };
+        in
+          builtins.toJSON entries;
+      };
+    };
   };
 
-  home.file = {
-    ".config/data/launcher.json" = {
-      enable = !config.conf.headless;
+  options.packages.ags-wrapped = lib.mkOption {
+    type = lib.types.package;
+    default = pkgs.symlinkJoin {
+      name = "ags-wrapped";
+      paths = [
+        config.programs.fzf.package
+        config.programs.ags.package
+        config.wayland.windowManager.hyprland.package
+        pkgs.upower
+        pkgs.bashInteractiveFHS
+        pkgs.pulseaudio
+        pkgs.toybox
+      ];
 
-      text = with pkgs; let
-        broPleaseItsWaylandTrustMe = pgm: "DESKTOP_SESSION=hyprland XDG_SESSION_TYPE=wayland WAYLAND_DISPLAY=wayland-1 XDG_BACKEND=wayland ${pgm}";
-
-        # TODO: this probably shouldn't be in this file its hard to find
-        entries = {
-          "alacritty" = "${config.programs.alacritty.package}/bin/alacritty -e ${pkgs.zellij}/bin/zellij";
-          "code" =
-            broPleaseItsWaylandTrustMe
-            "${config.programs.vscode.package}/bin/code  --enable-features=UseOzonePlatform --ozone-platform=wayland";
-          "chrome" =
-            broPleaseItsWaylandTrustMe
-            "/bin/google-chrome  --enable-features=UseOzonePlatform --ozone-platform=wayland";
-          "qute" = "${config.programs.qutebrowser.package}/bin/qutebrowser";
-        };
-      in
-        builtins.toJSON entries;
+      buildInputs = [pkgs.makeWrapper];
+      postBuild = ''
+        wrapProgram $out/bin/ags --set PATH $out/bin
+      '';
     };
   };
 }
