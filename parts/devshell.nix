082ef35e3dc52b593343cf5417cc81eece50b251
oleina <oleina@google.com>
2024-12-22 19:50:33 -0800

    remove astral

--- parts/devshell.nix
+++ parts/devshell.nix
@@ -6,14 +6,6 @@
   }: {
     devShells.default = pkgs.mkShell {
       packages = [
-        (pkgs.symlinkJoin
-          {
-            name = "ags-node";
-            paths = [
-              inputs.ags2.packages.${system}.default
-              pkgs.nodejs_23
-            ];
-          })
       ];
     };
   };
