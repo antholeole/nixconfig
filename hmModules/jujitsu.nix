f6b9c9c17e29d9800d37dd636836978426c6ac20
oleina <oleina@google.com>
2025-01-09 09:51:44 -0800

    downgrade jujutsu due to bugs

--- hmModules/jujitsu.nix
+++ hmModules/jujitsu.nix
@@ -45,7 +45,7 @@
 in {
   programs.jujutsu = {
     enable = true;
-    package = pkgs-unstable.jujutsu;
+    package = pkgs.jujutsu;
 
     settings = {
       user = with config.conf; {inherit email name;};
