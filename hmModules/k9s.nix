ff87a1901fce67adfb5fab8d9a4d35198c21ba58
oleina <oleina@google.com>
2025-03-18 05:26:00 +0000

    add a theme to k9s

--- hmModules/k9s.nix
+++ hmModules/k9s.nix
@@ -1,9 +1,16 @@
-{...}: {
+{inputs, ...}: let
+  gruvbox-dark-medium = "gruvbox-dark-medium";
+in {
+  home.file.".config/k9s/skins/${gruvbox-dark-medium}" = {
+    enable = true;
+    source = "${inputs.self}/confs/k9s/gruvbox-dark-medium.yaml";
+  };
+
   programs.k9s = {
     enable = true;
 
     settings.k9s = {
-      skin = "gruvbox-dark";
+      skin = "gruvbox-dark-medium";
 
       ui = {
         headless = true;
