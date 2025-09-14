6dcdd74242960c0f1603543887d2b4412da6da17
anthony <antholeinik@gmail.com>
2025-09-14 14:05:33 -0700

    make vidoe editing its own module

--- hmModules/video-editing.nix
+++ hmModules/video-editing.nix
@@ -1,12 +1,14 @@
 {
-  lib,
   config,
+  lib,
   pkgs,
   pkgs-unstable,
   ...
 }: {
   home.packages = with pkgs;
     lib.mkIf (builtins.elem "video-editing" config.conf.features) [
+      audacity
+
       blender
       ffmpeg_6-full
       v4l-utils
