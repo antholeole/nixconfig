2404b746d5813cfac70f80322ac3dd2e2e1cd392
folu <luca.fondo@trai.ch>
2025-10-27 11:06:31 +0100

    Replace monitors config with my own

--- shared/monitors.nix
+++ shared/monitors.nix
@@ -1,25 +1,6 @@
 {
-  # default laptop screen
-  "eDP-1" = {
-    scale = 2.5;
-  };
-
-  # work external monitor
-  "Hewlett Packard HP Z27n CNK62212PK" = {
-    scale = 1.15;
-  };
-
-  "Dell Inc. DELL P3223QE FX9XWN3" = {
-    scale = 1.6;
-  };
-
   # personal monitor
-  "Dell Inc. DELL P3223QE 5Y1J2P3" = {
-    scale = 1.60;
-  };
-
-  # chicago monitor
-  "PNP(AMZ) G24M2020G H513001860" = {
-    scale = 1.1;
+  "HP Inc. HP 27o 3CQ8261RK3" = {
+    scale = 1;
   };
 }
