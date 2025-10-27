c8cba654e62b0226e9bd6cdd4be547469e6a698c
oleina <oleina@google.com>
2025-10-27 09:48:03 -0700

    try to be clever with monitors

--- shared/monitors.nix
+++ shared/monitors.nix
@@ -1,25 +1,35 @@
-{
-  # default laptop screen
-  "eDP-1" = {
-    scale = 2.5;
-  };
+let
+  dellMonitorScale = 2;
+  dellMonitorPrefix = "Dell Inc. DELL P3223QE";
+  knownDellMonitorsForCLOE = [
+    "G54J484"
+    "FX9XWN3"
+    "5Y1J2P3" # personal
+  ];
+in
+  (
+    with builtins;
+      listToAttrs (map (v: {
+          name = "${dellMonitorPrefix} ${v}";
+          value = {
+            scale = dellMonitorScale;
+          };
+        })
+        knownDellMonitorsForCLOE)
+  )
+  // {
+    # default laptop screen
+    "eDP-1" = {
+      scale = 2.5;
+    };
 
-  # work external monitor
-  "Hewlett Packard HP Z27n CNK62212PK" = {
-    scale = 1.15;
-  };
+    # work external monitor
+    "Hewlett Packard HP Z27n CNK62212PK" = {
+      scale = 1.15;
+    };
 
-  "Dell Inc. DELL P3223QE FX9XWN3" = {
-    scale = 1.6;
-  };
-
-  # personal monitor
-  "Dell Inc. DELL P3223QE 5Y1J2P3" = {
-    scale = 1.60;
-  };
-
-  # chicago monitor
-  "PNP(AMZ) G24M2020G H513001860" = {
-    scale = 1.1;
-  };
-}
+    # chicago monitor
+    "PNP(AMZ) G24M2020G H513001860" = {
+      scale = 1.1;
+    };
+  }
