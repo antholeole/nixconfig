4eaedbdd09d918a7cedf9e84e8ba1db85810fc9f
Luca Fondo <luca.fondo@trai.ch>
2025-10-27 09:28:13 +0100

    Comment out unused ssh config
    Signed-off-by: Luca Fondo <luca.fondo@trai.ch>

--- hmModules/ssh.nix
+++ hmModules/ssh.nix
@@ -1,13 +1,13 @@
 {self, ...}: {
   home = {
-    file.".ssh/id_ed25519.pub" = {
-      enable = true;
-      source = "${self}/assets/id_ed25519.pub";
-    };
+    # file.".ssh/id_ed25519.pub" = {
+    #   enable = true;
+    #   source = "${self}/assets/id_ed25519.pub";
+    # };
 
-    file.".ssh/config" = {
-      enable = true;
-      source = "${self}/assets/config";
-    };
+    # file.".ssh/config" = {
+    #   enable = true;
+    #   source = "${self}/assets/config";
+    # };
   };
 }
