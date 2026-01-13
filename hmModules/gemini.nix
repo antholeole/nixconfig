c9337708fbb78043b575a31a0455ad5214250580
oleina <oleina@google.com>
2026-01-12 20:52:11 -0800

    use gemini off of master

--- hmModules/gemini.nix
+++ hmModules/gemini.nix
@@ -2,7 +2,6 @@
   pkgs,
   pkgs-unstable,
   config,
-  inputs,
   ...
 }: {
   programs.gemini-cli = {
@@ -10,11 +9,35 @@
     package = pkgs.symlinkJoin {
       name = "gemini-cli";
       buildInputs = [pkgs.makeWrapper];
-      paths = [
+      paths = let
+        # https://github.com/google-gemini/gemini-cli/issues/6146
+        gemini-master = with pkgs-unstable;
+          gemini-cli.overrideAttrs (finalAttrs: prevAttrs: {
+            version = "0.23";
+
+            src = fetchFromGitHub {
+              owner = "google-gemini";
+              repo = "gemini-cli";
+              tag = "v${}";
+              rev = "6adae9f7756d8efbc4c5901fe5884fa4a35f1f68";
+              hash = "sha256-G1H1M0iYzZkBwwG9KLmqETvacLT0V+9u3ktrgIti2xQ=";
+              postFetch = ''
+                ${lib.getExe npm-lockfile-fix} $out/package-lock.json
+              '';
+            };
+
+            npmDeps = fetchNpmDeps {
+              inherit (finalAttrs) src;
+              hash = "sha256-6PnZlhHk70aPmhIdRIt75rDIQrvZ7aSu6lGJXM6lRjU=";
+            };
+
+            dontCheckForBrokenSymlinks = true;
+          });
+      in [
         (
-          if config.conf.work
+          if builtins.elem "gbin" config.conf.features
           then pkgs.writeShellScriptBin "gemini" "/google/bin/releases/gemini-cli/tools/gemini"
-          else pkgs-unstable.gemini-cli
+          else gemini-master
         )
       ];
 
