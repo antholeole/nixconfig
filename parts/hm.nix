90403946f392a6989f22203f6804fdc9fa65e141
oleina <oleina@google.com>
2026-02-17 18:21:55 +0000

    rm stylix double import on raw hm

--- parts/hm.nix
+++ parts/hm.nix
@@ -22,7 +22,6 @@
           ++ moreModules
           ++ [
             inputs.oleina-shell.homeModules.default
-            inputs.stylix.nixosModules.stylix
 
             (import "${inputs.self}/hmModules/configs/${config}.nix")
           ];
